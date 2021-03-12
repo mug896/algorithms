0356f04770f389f75c7dd26df20f25c5b89563dc
mug896 <mug896@gmail.com>
2021-03-12 12:59:11 +0900

    update

--- red_black_tree.c
+++ red_black_tree.c
@@ -526,6 +526,7 @@ Insert + Search + Delete : %.5f seconds\n", time_str, time_total)
 
 //int data[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
 #include "randoms.h"
+//#include "seqs.h"
 
 rb_node *arr [1000000];
 
